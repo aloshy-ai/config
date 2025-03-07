9615a5fc4f3612b34b034d2b2e72ce14c9d17caf
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-03-07 04:54:09 +0000

    Enhance Homebrew configuration in default.nix with auto-update, cleanup options, and additional casks and MAS apps

--- nix/modules/darwin/brew/default.nix
+++ nix/modules/darwin/brew/default.nix
@@ -29,4 +29,37 @@
       "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
     };
   };
+
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+      cleanup = "zap";
+    };
+
+    brews = [
+      "nvm"
+    ];
+
+    casks = [
+      "cursor"
+      "webstorm"
+      "tailscale"
+      "claude"
+      "docker"
+      "opera"
+    ];
+
+    # taps = [
+    #   "homebrew/services"
+    # ];
+
+    masApps = {
+      "CopyClip" = 595191960;
+      "WireGuard" = 1451685025;
+      "DisplayMyIP" = 1493408723;
+    };
+  };
 }
